6ecb206f9183496c06732cd7d509406d72811a7a
Santiago Cabrera <santiago.cabrera.silva@gmail.com>
2018-12-04 20:38:51 +0000

    Added Heap generator

--- Functional Program Design in Scala/Week 3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ Functional Program Design in Scala/Week 3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,7 +9,7 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(const(empty), insert(arbitrary[Int], genHeap))
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
